8cf7dc168e96fea746448d61a87e43b0cb6a9930
Michel-Mendoza <mxndzaalol@gmail.com>
2021-03-28 20:01:20 +0200

    Update historial.js

--- cmds/historial.js
+++ cmds/historial.js
@@ -35,43 +35,19 @@ module.exports = {
         let history = await getHistory.get(region, (await summonerData).accountId, startIndex); if (getHistory == false) return (await msg).edit(notEnough);
         let version = ddragonver.get();
         let mark = "```";
-    
-        (await msg).edit({
-            content: null,
-            embed: {
-            color: null,
-            author: {
-                name: `Página ${page} del historial de partidas - ${(await summonerData).name}`,
-                icon_url: `https://ddragon.leagueoflegends.com/cdn/${await version}/img/profileicon/${(await summonerData).profileIconId}.png`
-            },
-            title: `Esto es lo que he encontrado:`,
-            fields: [{
-                name: `Partida ${page*5-4} - ${history.game1.remake?'Remake':((history.game1.win)?'Victoria':'Derrota')} - ${history.game1.duration}`,
-                value: `${mark}${history.game1.champ} - ${history.game1.kda} - ${history.game1.cs} minions. - ${history.game1.cola}\nID De la partida: ${history.game1.id}${mark}`,
-            },
-            {
-                name: `Partida ${page*5-3} - ${history.game2.remake?'Remake':((history.game2.win)?'Victoria':'Derrota')} - ${history.game2.duration}`,
-                value: `${mark}${history.game2.champ} - ${history.game2.kda} - ${history.game2.cs} minions. - ${history.game2.cola}\nID De la partida: ${history.game2.id}${mark}`,
-            },
-            {
-                name: `Partida ${page*5-2} - ${history.game3.remake?'Remake':((history.game3.win)?'Victoria':'Derrota')} - ${history.game3.duration}`,
-                value: `${mark}${history.game3.champ} - ${history.game3.kda} - ${history.game3.cs} minions. - ${history.game3.cola}\nID De la partida: ${history.game3.id}${mark}`,
-            },
-            {
-                name: `Partida ${page*5-1} - ${history.game4.remake?'Remake':((history.game4.win)?'Victoria':'Derrota')} - ${history.game4.duration}`,
-                value: `${mark}${history.game4.champ} - ${history.game4.kda} - ${history.game4.cs} minions. - ${history.game4.cola}\nID De la partida: ${history.game4.id}${mark}`,
-            },
-            {
-                name: `Partida ${page*5} - ${history.game5.remake?'Remake':((history.game5.win)?'Victoria':'Derrota')} - ${history.game5.duration}`,
-                value: `${mark}${history.game5.champ} - ${history.game5.kda} - ${history.game5.cs} minions. - ${history.game5.cola}\nID De la partida: ${history.game5.id}${mark}`,
-            },
-            ],
-                footer: {
-                    icon_url: message.author.avatarURL(),
-                    text: `Usa s.historial ${platform.toUpperCase()} <número de página> ${(await summonerData).name} para ver más partidas.`
-                } 
-            }
-        })
+
+        const dc = require('discord.js');
+        const embed = new dc.MessageEmbed();
+        embed.setTitle('Esto es lo que he encontrado:')
+        embed.setAuthor(`Página ${page} del historial de partidas - ${(await summonerData).name}`,`https://ddragon.leagueoflegends.com/cdn/${await version}/img/profileicon/${(await summonerData).profileIconId}.png`)
+        embed.addField(`Partida ${page*5-4} - ${history.game1.remake?'Remake':((history.game1.win)?'Victoria':'Derrota')} - ${history.game1.duration}`, `${mark}${history.game1.champ} - ${history.game1.kda} - ${history.game1.cs} minions. - ${history.game1.cola}\nID De la partida: ${history.game1.id}${mark}`)
+        embed.addField(`Partida ${page*5-3} - ${history.game2.remake?'Remake':((history.game2.win)?'Victoria':'Derrota')} - ${history.game2.duration}`, `${mark}${history.game2.champ} - ${history.game2.kda} - ${history.game2.cs} minions. - ${history.game2.cola}\nID De la partida: ${history.game2.id}${mark}`)
+        embed.addField(`Partida ${page*5-2} - ${history.game3.remake?'Remake':((history.game3.win)?'Victoria':'Derrota')} - ${history.game3.duration}`, `${mark}${history.game3.champ} - ${history.game3.kda} - ${history.game3.cs} minions. - ${history.game3.cola}\nID De la partida: ${history.game3.id}${mark}`)
+        embed.addField(`Partida ${page*5-1} - ${history.game4.remake?'Remake':((history.game4.win)?'Victoria':'Derrota')} - ${history.game4.duration}`, `${mark}${history.game4.champ} - ${history.game4.kda} - ${history.game4.cs} minions. - ${history.game4.cola}\nID De la partida: ${history.game4.id}${mark}`)
+        embed.addField(`Partida ${page*5-0} - ${history.game5.remake?'Remake':((history.game5.win)?'Victoria':'Derrota')} - ${history.game5.duration}`, `${mark}${history.game5.champ} - ${history.game5.kda} - ${history.game5.cs} minions. - ${history.game5.cola}\nID De la partida: ${history.game5.id}${mark}`)
+        embed.setFooter(`Usa s.historial <número de página> ${platform.toUpperCase()} ${(await summonerData).name} para ver más partidas.`, message.author.avatarURL())
+        embed.setTimestamp(new Date)
+        (await msg).edit(embed)
     }
 
 }
