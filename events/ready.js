3b024488c9f306891658db8f270e0bbf77aaed9c
Michel-Mendoza <mxndzaalol@gmail.com>
2021-03-28 20:36:36 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
     once: true,
 	async execute(client) {
-        client.user.setActivity(`desarrollando SettBot v3`, {type: 'PLAYING',})
+        client.user.setActivity(`versión v3 en desarrollo. ¡Nuevas funciones próximamente!`, {type: 'WATCHING',})
         console.log(`Bot listo.`)
         const servers = await client.guilds.cache.size
         const owner = await client.users.fetch('797254248387444769');
