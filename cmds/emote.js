48ffdaf3d63b0e304d3189512b378f2ade744933
Michel-Mendoza <mxndzaalol@gmail.com>
2021-03-29 18:10:27 +0200

    Update emote.js

--- cmds/emote.js
+++ cmds/emote.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'emote',
     description: 'test',
     async execute(message, args, client) {
-        const emote = client.emojis.find(emoji => emoji.name === args[0]);
+        const emotes = client.emojis
+        const emote = emotes.find(emoji => emoji.name === args[0]);
         console.log(emote)
     }
 }
