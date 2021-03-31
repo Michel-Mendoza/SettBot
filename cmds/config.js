f626199f6577c0d21e93eda1c30e097df8a26a5e
Michel-Mendoza <mxndzaalol@gmail.com>
2021-03-31 18:16:55 +0200

    Update config.js

--- cmds/config.js
+++ cmds/config.js
@@ -6,6 +6,7 @@ module.exports = {
             locale: 'en_US',
             economy: false
         })
+        let msg = require("../mensajes/config.js")
         if (!args[0]||!args[1]) {
             let obj = db.get(`${message.guild.id}`)
             if (obj.locale==='es_ES') message.reply(msg.missingArgs.es_ES)
@@ -17,7 +18,6 @@ module.exports = {
             let obj = db.get(`${message.guild.id}`)
             obj.locale = args[1]
             obj.save()
-            let msg = require("../mensajes/config.js")
             if (args[1]==='es_ES') message.reply(msg.es_ES)
             if (args[1]==='en_US') message.reply(msg.en_US)
             if (args[1]==='fr_FR') message.reply(msg.fr_FR)
