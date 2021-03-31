25752bec73410e1f9d35d917d458514103d55e40
Michel-Mendoza <mxndzaalol@gmail.com>
2021-03-31 18:30:54 +0200

    eco

--- cmds/config.js
+++ cmds/config.js
@@ -7,21 +7,34 @@ module.exports = {
             economy: false
         })
         let msg = require("../mensajes/config.js")
+        let obj = db.get(`${message.guild.id}`)
         if (!args[0]||!args[1]) {
-            let obj = db.get(`${message.guild.id}`)
             if (obj.locale==='es_ES') message.reply(msg.missingArgs.es_ES)
             if (obj.locale==='en_US') message.reply(msg.missingArgs.en_US)
             if (obj.locale==='fr_FR') message.reply(msg.missingArgs.fr_FR)
             return
         }
         if (args[0].toLowerCase()==='locale'&&(args[1]==='es_ES'||args[1]==='en_US'||args[1]==='fr_FR')) {
-            let obj = db.get(`${message.guild.id}`)
             obj.locale = args[1]
             obj.save()
             if (args[1]==='es_ES') message.reply(msg.locale.es_ES)
             if (args[1]==='en_US') message.reply(msg.locale.en_US)
             if (args[1]==='fr_FR') message.reply(msg.locale.fr_FR)
             return
+        } else if (args[0].toLowerCase()==='economy'&&(args[1]==='on'||args[1]==='off')) {
+            if (args[1]==='on') {
+                obj.economy = true;
+                obj.save()
+                if (obj.locale==='es_ES') message.reply(msg.economy.enable.es_ES)
+                if (obj.locale==='en_US') message.reply(msg.economy.enable.en_US)
+                if (obj.locale==='fr_FR') message.reply(msg.economy.enable.fr_FR)
+            } else if (args[1]==='off') {
+                obj.economy = false;
+                obj.save()
+                if (obj.locale==='es_ES') message.reply(msg.economy.disable.es_ES)
+                if (obj.locale==='en_US') message.reply(msg.economy.disable.en_US)
+                if (obj.locale==='fr_FR') message.reply(msg.economy.disable.fr_FR)
+            }
         }
     }
 }
--- mensajes/config.js
+++ mensajes/config.js
@@ -6,8 +6,19 @@ module.exports = {
         fr_FR: 'vous devez spécifier la valeur du paramètre que vous souhaitez modifier.',
     },
     locale: {
-        es_ES: 'el lenguaje ha sido cambiado correctamente a `es_ES (Español).`',
+        es_ES: 'el lenguaje ha sido cambiado correctamente a `es_ES (Español)`',
         en_US: 'the language has been successfully set to `en_US (English)`',
         fr_FR: 'la langue a été modifiée avec succès en `fr_FR (Français)`'
+    }, 
+    economy: {
+        enable: {
+            es_ES: 'el módulo de economía ha sido correctamente habilitado.',
+            en_US: 'the economics commands have been successfully enabled.',
+            fr_FR: 'le module économie a été activé avec succès.'
+        }, disable: {
+            es_ES: 'el módulo de economía ha sido correctamente deshabilitado.',
+            en_US: 'the economy commands have been successfully disabled.',
+            fr_FR: 'le module économie a été désactivé avec succès.'
+        }
     }
 }
