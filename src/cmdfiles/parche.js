16dbb8bd7f8f8deb623ac313d2907daa8239aed2
Michel-Mendoza <mxndzaalol@gmail.com>
2021-06-27 14:24:28 +0200

    Create parche.js

---
+++ src/cmdfiles/parche.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch')
+module.exports = {
+    name: "help",
+    description: "Muestra las notas del parche actual.",
+    options: [], msg: async (args, interaction, client) => {
+        let version = await fetch(`https://ddragon.leagueoflegends.com/api/versions.json`);
+        version = await version.json();
+        version = version[0].split('.')
+        while (version.length >= 3) version.pop()
+        version = version.join('-') 
+        let parche = `https://euw.leagueoflegends.com/es-es/news/game-updates/patch-${version}-notes/`
+        return parche
+    }
+};
