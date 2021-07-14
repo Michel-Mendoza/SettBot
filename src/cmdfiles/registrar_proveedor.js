06dd30a701d138992b75848aea216aa4df4c485a
Michel-Mendoza <73248365+Michel-Mendoza@users.noreply.github.com>
2021-07-14 16:08:19 +0200

    Update registrar_proveedor.js

--- src/cmdfiles/registrar_proveedor.js
+++ src/cmdfiles/registrar_proveedor.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch');
-const { URL } = require("url");
+const {
+    URL
+} = require("url");
 
 module.exports = {
     name: 'registrar_proveedor',
     description: 'Registra un nuevo proveedor de torneos.',
-    options: [
-        {    
+    options: [{
         name: "region",
         description: "La región en la que realizarás los torneos.",
         type: 3,
         required: true,
-        choices: [
-            {
+        choices: [{
                 name: "Europe West (EUW)",
                 value: "EUW"
             },
@@ -46,32 +46,41 @@ module.exports = {
             {
                 name: "Oceania (OCE)",
                 value: "OCE"
-            },{
+            }, {
                 name: "PBE",
                 value: "PBE"
             }
         ]
-        }],
+    }],
     msg: async (args, interaction, client) => {
-        let region = args[0].value 
+        let region = args[0].value
 
         if (!interaction.member.id == '797254248387444769') return 'No estás autorizado.'
 
-        let provider_register = {"region": region,"url": "https://discord.com/"}
+        let provider_register = {
+            "region": region,
+            "url": "https://discord.com/"
+        }
 
         let key = 'RGAPI-e6b78cc5-6d96-437f-b640-67f7e8dd7777'
 
         let api = new URL('https://americas.api.riotgames.com/lol/tournament-stub/v4/providers')
-        let data = await fetch(api, {headers: {'X-Riot-Token':key}, method: 'POST', body: JSON.stringify(provider_register)})
+        let data = await fetch(api, {
+            headers: {
+                'X-Riot-Token': key
+            },
+            method: 'POST',
+            body: JSON.stringify(provider_register)
+        })
         data = await data.json()
 
         const Discord = require('discord.js')
         let embed = new Discord.MessageEmbed()
             .setColor('RED')
             .setTitle('SettBot Torneos')
-            .setDescription('Un nuevo proveedor ha sido registrado.\nID del proveedor: '+ data)
+            .setDescription('Un nuevo proveedor ha sido registrado.\nID del proveedor: ' + data)
             .setTimestamp()
         return embed
     }
-    
+
 }
